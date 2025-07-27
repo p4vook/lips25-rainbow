981f0f826bdd722a1691223dd7f3da9206ddebf0
Pavel Kalugin <kalugin.pe@phystech.edu>
2025-07-27 14:22:45 +0300

    update flake

---
+++ flake.nix
@@ -0,0 +1,24 @@
+{
+  description = "A very basic flake";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+  };
+
+  outputs = { self, nixpkgs }:
+  let 
+  system = "aarch64-darwin";
+  pkgs = nixpkgs.legacyPackages."${system}";
+  llvm = pkgs.llvmPackages_20;
+  in {
+    devShells."${system}".default = pkgs.mkShell.override { stdenv = pkgs.gcc14Stdenv; } {
+      packages = with pkgs; [
+        git
+        pkgs.gdb
+        pkgs.hyperfine
+        llvm.libstdcxxClang
+        llvm.clang-tools
+      ];
+    };
+  };
+}
